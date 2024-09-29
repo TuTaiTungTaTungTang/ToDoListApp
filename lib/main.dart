014523c8b2926560f241ad5070b492d22c5eea11
TuTaiTungTaTungTang <taib2105559@student.ctu.edu.vn>
2024-09-29 11:52:37 +0700

    fix main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,17 @@
-import 'package:ct484_project/ui/cart/cart_screen.dart';
-import 'package:ct484_project/ui/products/user_products_screen.dart';
 import 'package:flutter/material.dart';
-import 'ui/products/products_manager.dart';
-import 'ui/products/product_detail_screen.dart';
-import 'ui/products/products_overview_screen.dart';
-import 'ui/orders/orders_screen.dart';
-void main(){
+
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    final colorScheme = ColorScheme.fromSeed(
-      seedColor: Colors.purple,
-      secondary: Colors.deepOrange,
-      surface: Colors.white,
-      surfaceTint: Colors.grey[200]);
-
-    final themeData = ThemeData(
-      fontFamily: 'Lato',
-      colorScheme: colorScheme,
-      appBarTheme: AppBarTheme(
-        backgroundColor: colorScheme.primary,
-        foregroundColor: colorScheme.onPrimary,
-        shadowColor: colorScheme.shadow,
-        elevation: 4,
-      ),
-      dialogTheme: DialogTheme(
-        titleTextStyle: TextStyle(
-          color: colorScheme.onSurface,
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-        ),
-        contentTextStyle: TextStyle(
-          color: colorScheme.onSurface,
-          fontSize: 20,
-        ),
-      ),
+    return MaterialApp(
+      title: 'ToDo App',
+      home: MyApp(),
     );
-
-     return MaterialApp(
-      title: 'MyShop',
-      debugShowCheckedModeBanner: false,
-      theme: themeData,
-      
-       home: const ProductsOverviewScreen(),
-      routes: {
-        CartScreen.routeName: (ctx) => const SafeArea(
-              child: CartScreen(),
-            ),
-        OrdersScreen.routeName: (ctx) => const SafeArea(
-              child: OrdersScreen(),
-            ),
-        UserProductsScreen.routeName: (ctx) => const SafeArea(
-              child: UserProductsScreen(),
-            ),
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == ProductDetailScreen.routeName) {
-          final productId = settings.arguments as String;
-          return MaterialPageRoute(
-            settings: settings,
-            builder: (ctx) {
-              return SafeArea(
-                child: ProductDetailScreen(
-                  ProductsManager().findById(productId)!,
-                ),
-              );
-            },
-          );
-        }
-        return null;
-      },
-     );
   }
 }
