7f48c7ed35234572110406dfc954e25dc1ed6cad
TuTaiTungTaTungTang <taib2105559@student.ctu.edu.vn>
2024-10-05 01:25:16 +0700

    Tách các logic trong home.dart ra thành các hàm nhỏ hơn.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import '../models/todo.dart';
 import '../constants/colors.dart';
 import '../widgets/todo_item.dart';
+import '../widgets/search_box.dart';
+import '../widgets/add_todo.dart';
+import '../services/todo_service.dart';
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -12,11 +15,13 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final todosList = ToDo.todoList();
+  late ToDoService toDoService;
   List<ToDo> _foundToDo = [];
   final _todoController = TextEditingController();
 
   @override
   void initState() {
+    toDoService = ToDoService(todosList: todosList);
     _foundToDo = todosList;
     super.initState();
   }
@@ -29,106 +34,40 @@ class _HomeState extends State<Home> {
       body: Stack(
         children: [
           Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 15,
-            ),
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
             child: Column(
               children: [
-                searchBox(),
+                SearchBox(onChanged: _runFilter),
                 Expanded(
-                  child: _foundToDo.isNotEmpty
-                      ? ListView(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.only(
-                                top: 50,
-                                bottom: 20,
-                              ),
-                              child: Text(
-                                'All ToDos',
-                                style: TextStyle(
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ),
-                            for (ToDo todoo in _foundToDo.reversed)
-                              ToDoItem(
-                                todo: todoo,
-                                onToDoChanged: _handleToDoChange,
-                              ),
-                          ],
-                        )
-                      : Center(
-                          child: Text(
-                            'Không tìm thấy công việc nào',
-                            style: TextStyle(fontSize: 18, color: tdGrey),
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 50, bottom: 20),
+                        child: Text(
+                          'All ToDos',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
                           ),
                         ),
+                      ),
+                      for (ToDo todo in _foundToDo.reversed)
+                        ToDoItem(
+                          todo: todo,
+                          onToDoChanged: _handleToDoChange,
+                          onDeleteItem: _deleteToDoItem,
+                        ),
+                    ],
+                  ),
                 )
               ],
             ),
           ),
           Align(
             alignment: Alignment.bottomCenter,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    margin: EdgeInsets.only(
-                      bottom: 20,
-                      right: 20,
-                      left: 20,
-                    ),
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 20,
-                      vertical: 5,
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: const [
-                        BoxShadow(
-                          color: Colors.grey,
-                          offset: Offset(0.0, 0.0),
-                          blurRadius: 10.0,
-                          spreadRadius: 0.0,
-                        ),
-                      ],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: TextField(
-                      controller: _todoController,
-                      decoration: InputDecoration(
-                        hintText: 'Add a new todo item',
-                        border: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(
-                    bottom: 20,
-                    right: 20,
-                  ),
-                  child: ElevatedButton(
-                    child: Text(
-                      '+',
-                      style: TextStyle(
-                        fontSize: 40,
-                      ),
-                    ),
-                    onPressed: () {
-                      _addToDoItem(_todoController.text);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: tdBlue,
-                      minimumSize: Size(60, 60),
-                      elevation: 10,
-                    ),
-                  ),
-                ),
-              ],
+            child: AddToDoWidget(
+              todoController: _todoController,
+              onAddPressed: () => _addToDoItem(_todoController.text),
             ),
           ),
         ],
@@ -138,65 +77,27 @@ class _HomeState extends State<Home> {
 
   void _handleToDoChange(ToDo todo) {
     setState(() {
-      todo.isDone = !todo.isDone;
+      toDoService.handleToDoChange(todo);
     });
   }
 
-  void _addToDoItem(String toDo) {
-    if (toDo.isNotEmpty) {
-      setState(() {
-        todosList.add(ToDo(
-          id: DateTime.now().millisecondsSinceEpoch.toString(),
-          todoText: toDo,
-        ));
-      });
-      _todoController.clear();
-    }
+  void _deleteToDoItem(String id) {
+    setState(() {
+      toDoService.deleteToDoItem(id);
+    });
+  }
+
+  void _addToDoItem(String toDoText) {
+    setState(() {
+      toDoService.addToDoItem(toDoText);
+    });
+    _todoController.clear();
   }
 
   void _runFilter(String enteredKeyword) {
-    List<ToDo> results = [];
-    if (enteredKeyword.isEmpty) {
-      results = todosList;
-    } else {
-      results = todosList
-          .where((item) => item.todoText!
-              .toLowerCase()
-              .contains(enteredKeyword.toLowerCase()))
-          .toList();
-    }
-
     setState(() {
-      _foundToDo = results;
+      _foundToDo = toDoService.runFilter(enteredKeyword);
     });
-  }
-
-  Widget searchBox() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 15),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: TextField(
-        onChanged: (value) => _runFilter(value),
-        decoration: InputDecoration(
-          contentPadding: EdgeInsets.all(0),
-          prefixIcon: Icon(
-            Icons.search,
-            color: tdBlack,
-            size: 20,
-          ),
-          prefixIconConstraints: BoxConstraints(
-            maxHeight: 20,
-            minWidth: 25,
-          ),
-          border: InputBorder.none,
-          hintText: 'Search',
-          hintStyle: TextStyle(color: tdGrey),
-        ),
-      ),
-    );
   }
 
   AppBar _buildAppBar() {
@@ -216,8 +117,7 @@ class _HomeState extends State<Home> {
             width: 40,
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
-              child: Image.asset(
-                  'assets/images/z5454229034776_1d84ce7f2709be82c56d968a422066ef.jpg'),
+              child: Image.asset('assets/images/avatar.jpeg'),
             ),
           ),
         ],
