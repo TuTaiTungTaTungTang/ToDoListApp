a75e2b3dbb514a701e43f5263d99a16f9d0beb99
TuTaiTungTaTungTang <taib2105559@student.ctu.edu.vn>
2024-10-05 22:54:34 +0700

    Thay đổi màu nền cho AppBar thành tdGrey. Căn giữa tiêu đề "To Do List" và điều chỉnh kích thước chữ để to hơn và đậm hơn. Thêm hình ảnh vào AppBar, cho phép chuyển hướng đến trang UserPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:ct484_project/constants/colors.dart';
 import 'package:flutter/material.dart';
 import './screens/home.dart';
+import './screens/user.dart'; // Đảm bảo import trang User
 import './shared/app_drawer.dart';
 
 void main() {
@@ -15,9 +17,40 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'ToDo App',
       home: Scaffold(
-        appBar: AppBar(title: Text('ToDo List')),
-        drawer: AppDrawer(), // Sử dụng AppDrawer ở đây
-        body: Home(), // Thay thế bằng widget Home của bạn
+        appBar: AppBar(
+          backgroundColor: tdBGColor,
+          title: Center(
+            child: Text(
+              'To Do List',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24, // Kích thước chữ lớn hơn
+              ),
+            ),
+          ),
+          actions: [
+            GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => UserPage(), // Chuyển đến trang User
+                  ),
+                );
+              },
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20), // Bo góc
+                child: Image.asset(
+                  'assets/images/z5454229034776_1d84ce7f2709be82c56d968a422066ef.jpg', // Thay đường dẫn hình ảnh của bạn
+                  height: 40, // Chiều cao hình ảnh
+                  width: 40, // Chiều rộng hình ảnh
+                ),
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: tdBGColor,
+        drawer: AppDrawer(),
+        body: Home(),
       ),
     );
   }
